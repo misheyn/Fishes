d38386802038c9b9f74c4bf4da477cb08c2d7fa2
0ldStar <brosto_man@mail.ru>
2022-04-01 09:03:03 +0700

    add alert, set default value for comboBoxes and textFields

--- src/main/java/com/company/JavaFish/StartMenuController.java
+++ src/main/java/com/company/JavaFish/StartMenuController.java
@@ -6,10 +6,7 @@ import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.DialogPane;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 public class StartMenuController {
 
@@ -32,6 +29,7 @@ public class StartMenuController {
 
     @FXML
     void exitMenuButtonClick(ActionEvent event) {
+        int flag = 1;
         try {
             Habitat.setP1(Integer.parseInt(goldenFishComboBox.getValue()));
         } catch (NumberFormatException e) {
@@ -42,20 +40,28 @@ public class StartMenuController {
         } catch (NumberFormatException e) {
             Habitat.setP2(60);
         }
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Wrong input!\nThink about it ☺", ButtonType.YES);
         try {
             Habitat.setN1(Integer.parseInt(goldenFishTextField.getText()));
         } catch (NumberFormatException e) {
+            alert.showAndWait();
+            flag = 0;
             Habitat.setN1(5);
+            goldenFishTextField.setText("5");
         }
         try {
             Habitat.setN2(Integer.parseInt(guppyFishTextField.getText()));
         } catch (NumberFormatException e) {
+            alert.showAndWait();
+            flag = 0;
+            guppyFishTextField.setText("2");
             Habitat.setN2(2);
         }
-
-        startMenu.setVisible(false);
-        startMenu.setDisable(true);
-        StartMenu.getInstance().getStage().close();
+        if (flag == 1) {
+            startMenu.setVisible(false);
+            startMenu.setDisable(true);
+            StartMenu.getInstance().getStage().close();
+        }
     }
 
     @FXML
@@ -65,6 +71,10 @@ public class StartMenuController {
             comboBoxTexts.add(Integer.toString(i));
         goldenFishComboBox.getItems().addAll(comboBoxTexts);
         guppyFishComboBox.getItems().addAll(comboBoxTexts);
+        goldenFishComboBox.getSelectionModel().select(3);
+        guppyFishComboBox.getSelectionModel().select(5);
+        goldenFishTextField.setText("5");
+        guppyFishTextField.setText("2");
         startMenu.setContent(null);
         startMenu.setGraphic(null);
     }
